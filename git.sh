b29a9e95e3305ad5d3f36aa524f3020e011d0fdb
yildizm <yildizm@outlook.com>
2017-05-07 18:35:46 +0300

    git_pusher_fixed

--- git.sh
+++ git.sh
@@ -7,6 +7,6 @@ echo "Things are pushed to github. Changelog:$1"
 git submodule foreach git pull origin master
 git add .
 git commit -m "$1"
-git push origin master
+git push origin $2
 echo "copying."
 cp -r turjeeman-client/src/ static
