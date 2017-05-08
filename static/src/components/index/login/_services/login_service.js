7d43377f575ef6722c6b6d1996585d1697923dfa
Ali Kavis <alikavis@gmail.com>
2017-05-08 06:47:36 +0000

    client views

--- static/src/components/index/login/_services/login_service.js
+++ static/src/components/index/login/_services/login_service.js
@@ -4,36 +4,7 @@ import appState from "../../../../utility/app_state";
 class LoginService {
     loginUser(user = {}) {
         return Promise.try(() => {
-		let {password, username} = user;
-		fetch('auth/data', {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				username : {username},
-				password : {password},
-			})			
-		})/*
-		fetch('auth/data', {
-				method: 'POST', 
-				headers: { 
-					'Accept': 'application/json', 
-					'Content-Type': 'application/json', 
-				}, 	
-				body:{
-					'username': this.username,
-					'password': this.password,
-					/*JSON.stringify({ 
-					firstParam: {username}, 
-					secondParam: {password},
-				}	
-//.replace(/{|}/gi, "")
-			}).then(response => response.json())
-			.then(response => {
-				console.log(response.json);
-			});*/
+            let {password, username} = user;
             if (password === "123456" && username === "deneme") {
                 let user = {username: "Baraa", name: "Baraa", surname: "Orabi"};
                 return appState.setUser(user);
