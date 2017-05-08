e9675f164749a989c216131e20ba5fa09490c3de
Ali Kavis <alikavis@gmail.com>
2017-05-08 06:44:42 +0000

    master

--- sentence/views.py
+++ sentence/views.py
@@ -1,6 +1,43 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+import xmlrpclib, json
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 # Create your views here.
+server_ip = '139.179.103.66'
+server_port = '8082'
+
+class sentence(View):
+	@method_decorator(csrf_exempt)
+	def dispatch(self, request, *args, **kwargs):
+		return super(sentence, self).dispatch(request, *args, **kwargs)
+
+	def parse_json_data(self, request_body):
+    json_object = json.loads(request_body)
+    return json_object
+
+    def post(self, request):
+    	json_obj = self.parse_json_data(request.body)
+		sentence_pairs = json_obj['sentence_pairs']
+
+		server_proxy_address = 'http://' + server_ip + ':' + server_port
+		sentencer_server_proxy = xmlrpclib.ServerProxy(server_proxy_address)
+
+		source_text = ''
+		target_text = ''
+
+		for s, t in sentence_pairs:
+		    source_text = source_text + s
+		    target_text = target_text + t
+
+		source_lang = json_obj['source_language']
+		target_lang = json_obj['target_language']
+
+		json_obj['sentence_pairs'] = sentencer_server_proxy.get_sentence_to_sentence_mapping(source_text, target_text, source_lang, target_lang)[0]
+		return JsonResponse(json_obj)
--- tokenizer/views.py
+++ tokenizer/views.py
@@ -1,6 +1,65 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+import xmlrpclib, json
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 # Create your views here.
+server_ip = '139.179.103.66'
+server_port = '8081'
+
+class tokenizer(View):
+	@method_decorator(csrf_exempt)
+	def dispatch(self, request, *args, **kwargs):
+		return super(tokenizer, self).dispatch(request, *args, **kwargs)
+
+	def parse_json_data(self, request_body):
+		json_object = json.loads(request_body)
+		return json_object
+
+	def get_string_tokens_from_index_pairs(self, sentence, token_idx_pairs):
+		ret_val = []
+		for pair in token_idx_pairs:
+			ret_val.append(sentence[pair[0]:pair[1]])
+
+		return ret_val
+
+	def generate_index_pairs_from_tokens(self, tokens, sentence):
+			'''Given all the tokens in order, the function generates the (start, end) index pair for each token'''
+
+			followup = 0
+			l = []
+			for t in tokens:
+				if t != '':
+					idx = sentence.find(t)
+					if idx != -1:
+						l.append((followup + idx, followup + idx + len(t)))
+						sentence = sentence[idx + len(t):]
+						followup += idx + len(t)
+			return l
+
+	def post(self, request):
+		json_obj = self.parse_json_data(request.body)
+		server_proxy_address = 'http://' + server_ip + ':' + server_port
+		tokenizer_server_proxy = xmlrpclib.ServerProxy(server_proxy_address)
+
+
+		sentence_pairs = json_obj['sentence_pairs']
+
+		all_tokens = []
+		for source_sent, target_sent in sentence_pairs:
+			tokens = []
+			s_t = tokenizer_server_proxy.tokenize(source_sent, json_obj['source_language'])
+			t_t = tokenizer_server_proxy.tokenize(target_sent, json_obj['target_language'])
+
+			tokens.append(self.generate_index_pairs_from_tokens(s_t, source_sent))
+			tokens.append(self.generate_index_pairs_from_tokens(t_t, target_sent))
+			all_tokens.append(tokens)
+
+		json_obj['tokens'] = all_tokens
+		return JsonResponse(json_obj)
