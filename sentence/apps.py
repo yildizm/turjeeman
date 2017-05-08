ed7b8aba005e3a57ebcd0ad19bb15445dc5b206c
yildizm <yildizm@outlook.com>
2017-05-08 06:26:11 +0300

    submodules added

--- sentence/apps.py
+++ sentence/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class SentencerConfig(AppConfig):
-    name = 'sentencer'
+class SentenceConfig(AppConfig):
+    name = 'sentence'
